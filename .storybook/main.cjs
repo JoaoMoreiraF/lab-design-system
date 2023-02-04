126c731a870860220f9276ff517d1bb1ed6852aa
JoaoMoreiraF <ferreira.moreira.joao@gmail.com>
2023-02-04 13:23:03 -0300

    remove additional config into main.cjs storybook file

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,12 +14,5 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
-  },
-  viteFinal: (config, { configType }) => {
-    if(configType === 'PRODUCTION') {
-      config.base = '/lab-design-system'
-    }
-
-    return config
   }
 }
